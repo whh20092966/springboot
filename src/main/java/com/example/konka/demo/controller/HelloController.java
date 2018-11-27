cd38132c6149edc8657577b7391f3c503ccc0cc5
whh20092966 <395055557@qq.com>
2018-11-27 23:37:32 +0800

    add hello api

---
+++ src/main/java/com/example/konka/demo/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.example.konka.demo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author hipi
+ */
+@RestController
+@RequestMapping("v1/hello")
+@Slf4j
+public class HelloController {
+
+
+    @GetMapping("")
+    public String hello(){
+        log.info("hello ---> spring boot");
+
+        return "hello devops spring boot !!!!";
+    }
+}
